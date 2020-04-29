3211c1a5991bafb2eea3dba7d9b26854d2dd60af
Jake G <jake098877@gmail.com>
2020-04-29 19:00:24 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,9 +2,10 @@ require 'pry'
 def prime?(number)
   num = number / 2
     i = number - 1
-# binding.pry
     while i > 0
     if number % i  == 0
+      binding.pry
+
       false
     end
     i -= 1
