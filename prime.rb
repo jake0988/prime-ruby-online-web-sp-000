3c443a92d9764b9e4eca765f4159a6467b4a6873
Jake G <jake098877@gmail.com>
2020-04-29 19:14:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,16 @@
 require 'pry'
 def prime?(number)
     i = number - 1
-    if number == -1
-      false
-    end
+
     while i > 1
     if number % i  == 0
 
       false
   end
     i -= 1
+  end
+  if number < 0
+    false
   end
   true
 end
