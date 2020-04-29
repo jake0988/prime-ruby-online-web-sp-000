5d6623b330eedbcda0e9634c999fc9d5868009f4
Jake G <jake098877@gmail.com>
2020-04-29 19:23:05 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,15 +3,16 @@ def prime?(number)
     i = number - 1
 
     while i > 1
-    if number % i  != 0
+      if number == 2
+        true
+      elsif number % i == 0
+      true
+    elsif number % i  != 0
 
       false
   elsif number < 2
     false
-  elsif number == 2
-    true
-  elsif number % i == 0
-  true
+
   end
     i -= 1
 
