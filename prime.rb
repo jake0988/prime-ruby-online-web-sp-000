19fce0c97cf88790acde1da51b7c5abc7723adb3
Jake G <jake098877@gmail.com>
2020-04-29 19:03:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,9 +3,10 @@ def prime?(number)
   num = number / 2
     i = number - 1
     while i > 1
-    if number % i  == 0
-      binding.pry
-
+    if number < 0
+      false
+    elsif number % i  == 0
+      # binding.pry
       false
     end
     i -= 1
